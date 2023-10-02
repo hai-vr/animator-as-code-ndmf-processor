b4e616de6d17e6a1f1c34f4cae8046f4f02d2e13
Haï~ <hai-vr@users.noreply.github.com>
2023-10-02 08:46:58 +0200

    Add NDMF As Code first draft

---
+++ Examples/NdmfAsCodeToggle.cs
@@ -0,0 +1,46 @@
+﻿#if UNITY_EDITOR
+using System;
+using ModularAvatarAsCode.V1;
+using nadena.dev.ndmf;
+using NdmfAsCode.V1.Example;
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+
+[assembly: ExportsPlugin(typeof(NdmfAsCodeToggleProcessor))]
+namespace NdmfAsCode.V1.Example
+{
+    public class NdmfAsCodeToggle : MonoBehaviour
+    {
+        public string parameter;
+        public GameObject[] objects;
+        public Texture2D icon;
+    }
+
+    public class NdmfAsCodeToggleProcessor : AbstractNdmfAsCodePlugin
+    {
+        protected override Type ScriptType => typeof(NdmfAsCodeToggle);
+
+        protected override NdmfAsCodeOutput Execute()
+        {
+            var my = (NdmfAsCodeToggle)script;
+            
+            var ctrl = aac.NewAnimatorController();
+            var fx = ctrl.NewLayer();
+            
+            var off = fx.NewState("OFF").WithAnimation(aac.NewClip().Toggling(my.objects, false));
+            var on = fx.NewState("ON").WithAnimation(aac.NewClip().Toggling(my.objects, true));
+
+            var param = fx.BoolParameter(my.parameter);
+            off.TransitionsTo(on).When(param.IsTrue());
+            on.TransitionsTo(off).When(param.IsFalse());
+
+            var maAc = MaAc.Create(script.gameObject);
+            maAc.NewMergeAnimator(ctrl, VRCAvatarDescriptor.AnimLayerType.FX);
+            maAc.NewParameter(param);
+            maAc.EditMenuItemOnSelf().Toggle(param).Name(my.gameObject.name).WithIcon(my.icon);
+
+            return NdmfAsCodeOutput.Regular();
+        }
+    }
+}
+#endif
---
+++ Packages/dev.hai-vr.animator-as-code.v1.ndmf-processor/V1/Editor/NdmfAsCode.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using AnimatorAsCode.V1;
+using nadena.dev.ndmf;
+using UnityEditor;
+using UnityEngine;
+
+namespace NdmfAsCode.V1
+{
+    // Can't be abstract
+    // public class AbstractNdmfAsCodePlugin<T> : Plugin<AbstractNdmfAsCodePlugin<T>> where T : MonoBehaviour
+    public class AbstractNdmfAsCodePlugin : Plugin<AbstractNdmfAsCodePlugin>
+    {
+        // Must be changed
+        protected virtual Type ScriptType => typeof(AbstractNdmfAsCodePlugin);
+        
+        // Can be changed if necessary
+        protected virtual string SystemName(Component script, BuildContext context) => GetType().Name;
+        protected virtual Transform AnimatorRoot(Component script, BuildContext context) => context.AvatarRootTransform;
+        protected virtual Transform DefaultValueRoot(Component script, BuildContext context) => context.AvatarRootTransform;
+        protected virtual bool UseWriteDefaults(Component script, BuildContext context) => false;
+
+        //
+        
+        protected AacFlBase aac;
+        protected Component script;
+        protected BuildContext buildContext;
+        
+        public override string QualifiedName => $"dev.hai-vr.ndmf-processor::{GetType().FullName}";
+        public override string DisplayName => $"NdmfAsCode for {GetType().Name}";
+
+        protected virtual NdmfAsCodeOutput Execute()
+        {
+            return NdmfAsCodeOutput.Regular();
+        }
+
+        protected override void Configure()
+        {
+            if (GetType() == typeof(AbstractNdmfAsCodePlugin)) return;
+            
+            InPhase(BuildPhase.Generating).Run($"Run NdmfAsCode for {GetType().Name}", ctx =>
+            {
+                var scripts = ctx.AvatarRootObject.GetComponentsInChildren(ScriptType, true);
+                foreach (var currentScript in scripts)
+                {
+                    aac = AacV1.Create(new AacConfiguration
+                    {
+                        SystemName = SystemName(currentScript, ctx),
+                        AnimatorRoot = AnimatorRoot(currentScript, ctx),
+                        DefaultValueRoot = DefaultValueRoot(currentScript, ctx),
+                        AssetKey = GUID.Generate().ToString(),
+                        GenericAssetContainer = ctx.AssetContainer,
+                        DefaultsProvider = new AacDefaultsProvider(UseWriteDefaults(currentScript, ctx))
+                    });
+                    script = currentScript;
+                    buildContext = ctx;
+                    Execute();
+                }
+            });
+        }
+    }
+
+    // public interface INdmfAsCodeProcessor<T> where T : MonoBehaviour
+    // {
+        // void Execute(T current, BuildContext x);
+    // }
+
+    public struct NdmfAsCodeOutput
+    {
+        public Motion[] directBlendTreeMembers;
+        public NdmfAsCodeWeightedDirectBlendTreeMember[] weightedMembers;
+
+        public static NdmfAsCodeOutput Regular()
+        {
+            return new NdmfAsCodeOutput
+            {
+                directBlendTreeMembers = Array.Empty<Motion>(),
+                weightedMembers = Array.Empty<NdmfAsCodeWeightedDirectBlendTreeMember>()
+            };
+        }
+
+        public static NdmfAsCodeOutput MergeIntoDirectBlendTree(Motion[] motionsToAddInSharedDirectBlendTree)
+        {
+            return new NdmfAsCodeOutput
+            {
+                directBlendTreeMembers = motionsToAddInSharedDirectBlendTree,
+                weightedMembers = Array.Empty<NdmfAsCodeWeightedDirectBlendTreeMember>()
+            };
+        }
+    }
+    
+    public struct NdmfAsCodeWeightedDirectBlendTreeMember
+    {
+        public AacFlFloatParameter parameter;
+        public Motion member;
+    }
+}
